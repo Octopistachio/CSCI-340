b6105eb69b5cb3e87bbed31c8b0612fe335d07af
Matt Wilson <mwilson2250@gmail.com>
2018-02-01 14:46:58 -0600

    Project Layout

--- SeamCarver/src/SeamCarver.java
+++ SeamCarver/src/SeamCarver.java
@@ -1,6 +1,32 @@
 public class SeamCarver {
-
-    public static void main(String[] args) {
+    // create a seam carver object based on the given picture
+    public SeamCarver(EasyBufferedImage picture) {
 
     }
+
+    // energy of pixel at column x and row y
+    public double getEnergy(int row, int col) {
+        return 0;
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return new int[]{0};
+    }
+
+    // sequence of indices for a horizontal seam
+    public int[] findHorizontalSeam() {
+        return new int[]{0};
+    }
+
+    // find and remove vertical seam from the picture
+    public void findAndRemoveVerticalSeam() {
+
+    }
+
+    // find and remove horizontal seam from the picture
+    public void findAndRemoveHorizontalSeam() {
+
+    }
+
 }
