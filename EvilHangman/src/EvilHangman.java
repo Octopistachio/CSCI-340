465936ca041d8f206328caefb8ec4587cf78e9c1
Matt Wilson <mwilson2250@gmail.com>
2018-03-20 15:42:39 -0500

    Worked more on FindFamilies()

--- EvilHangman/src/EvilHangman.java
+++ EvilHangman/src/EvilHangman.java
@@ -3,16 +3,15 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class EvilHangman {
 
 
-    private static ArrayList<String> library = new ArrayList<>();
-    private static ArrayList<String> lengthLibrary = new ArrayList<>(); //Library of all words that contain the length chosen
+    private static List<String> library = new ArrayList<>();
+    private static List<String> lengthLibrary = new ArrayList<>(); //Library of all words that contain the length chosen
     private static int maxWordLength = 0;
+    private static int availableWords = 0;
 
     public static void main(String args[]) {
 
@@ -71,6 +70,8 @@ public class EvilHangman {
                 library.add(line.toUpperCase());
                 if (line.length() > maxWordLength) maxWordLength = line.length();
             }
+
+
         } catch (FileNotFoundException ex) {
             System.err.println("Unable to open file '" + fileName + "'");
         } catch (IOException ex) {
@@ -87,10 +88,14 @@ public class EvilHangman {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
+            int total = 0;
             while ((line = bufferedReader.readLine()) != null) {
-                if (line.length() == wordLength)
+                if (line.length() == wordLength) {
+                    total++;
                     lengthLibrary.add(line.toUpperCase());
+                }
             }
+            availableWords = total;
         } catch (FileNotFoundException ex) {
             System.err.println("Unable to open file '" + fileName + "'");
         } catch (IOException ex) {
@@ -106,8 +111,12 @@ public class EvilHangman {
         Scanner sc = new Scanner(System.in);
         ArrayList<Character> guessedLetters = new ArrayList<>();
 
+
+        char[] word = new char[wordLength];
+
         while (guessCount < guessLength) {
-            print(wordLength, guessLength, showAvailableWords, guessCount, guessedLetters, findFamilies(wordLength, guessedLetters));
+            word = findFamilies(word, guessedLetters);
+            print(wordLength, guessLength, showAvailableWords, guessCount, guessedLetters, word);
             System.out.print("\nGuess a letter: ");
             String guessedLetterString = sc.next().toUpperCase();
             char guessedLetter = guessedLetterString.charAt(0);
@@ -129,72 +138,107 @@ public class EvilHangman {
 
     private static void print(int wordLength, int guessLength, boolean showAvailableWords, int guessCount, ArrayList<Character> guessedLetters, char[] word) {
 
-        word = new char[wordLength];
-
         System.out.println();
 
         if (showAvailableWords)
-            System.out.println("Possible words: ");
+            System.out.println("Possible words: " + availableWords);
         System.out.println("Guesses remaining: " + (guessLength - guessCount));
         System.out.println("Guessed letters: " + guessedLetters);
         System.out.print("Current state: ");
 
 
         for (int i = 0; i < wordLength; i++) {
-            if (word[i] == '\u0000') {
-                System.out.print("_ ");
-            } else {
-                System.out.print(word[i] + " ");
-            }
-        }
-    }
-
-    private static char[] findFamilies(int wordLength, ArrayList<Character> guessedLetters) {
-        char[] nothing = new char[wordLength];
-
-        ArrayList<String> notGuessedLetterLibrary = new ArrayList<>();
-        ArrayList<char[]> guessedLetterLibrary = new ArrayList<>();
-
-        char lastLetterGuessed = '\u0000';
-
-        if(guessedLetters.size() != 0)
-               lastLetterGuessed = guessedLetters.get(guessedLetters.size() - 1);
-
-        for (String word : lengthLibrary) {
-            boolean containsGuessedLetter = false;
-            for (int i = 0; i < word.length(); i++) {
-                if (word.charAt(i) == lastLetterGuessed) {
-                    containsGuessedLetter = true;
+            boolean foundMatch = false;
+            for(char letter:guessedLetters) {
+                if(word[i] == letter) {
+                    foundMatch = true;
+                    System.out.print(word[i] + " ");
                     break;
                 }
             }
-            char[] strippedWord = new char[wordLength];
-            if (containsGuessedLetter) {
-                for (int i = 0; i < word.length(); i++) {
-                    if (word.charAt(i) == lastLetterGuessed) {
-                        strippedWord[i] = lastLetterGuessed;
+
+            if(!foundMatch)
+                System.out.print("_ ");
+        }
+    }
+
+    private static char[] findFamilies(char[] currentWord, List<Character> guessedLetters) {
+
+        HashMap<String, List<String>> wordMap = new HashMap<>(); //<Pattern, Words>
+
+        char lastLetterGuessed = '\u0000'; //The last word guessed by the player
+
+        if(guessedLetters.size() != 0) { //If the player has guessed at least one letter
+            lastLetterGuessed = guessedLetters.get(guessedLetters.size() - 1); //Set the last letter guessed to the last letter in the array
+        }
+
+        for (String word:lengthLibrary) { //For each word in the library of words with the length chosen
+            boolean containsLastGuessedLetter = false; //Is true if the word contains the last letter guessed
+            boolean containsAlreadyGuessedLetter = false; //Is true if the word contains a letter that was already guessed
+            for (int i = 0; i < word.length(); i++) { //For each character in the word
+                if (word.charAt(i) == lastLetterGuessed) { //If the word contains the last letter guessed
+                    containsLastGuessedLetter = true; //Set the boolean to true
+                }
+                for(char letter:guessedLetters) { //For each letter that was already guessed
+                    if(word.charAt(i) == letter && letter != lastLetterGuessed) { //If the letter in the word is one of the already guessed letters, BUT NOT the last guessed letter
+                        containsAlreadyGuessedLetter = true; //Set the boolean to true
+                        break; //Break the loop
                     }
                 }
-                guessedLetterLibrary.add(strippedWord);
-
-            } else {
-                notGuessedLetterLibrary.add(word);
             }
-        }
+            if(!containsAlreadyGuessedLetter) { //If the word does NOT contain a letter that was already guessed
+                String strippedWord = word;
+                if (containsLastGuessedLetter) {
+                    for (int i = 0; i < word.length(); i++) {
+                        if (word.charAt(i) == lastLetterGuessed) {
+                            strippedWord += lastLetterGuessed;
+                        } else {
+                            strippedWord += "-";
+                        }
+                    }
+                    if (wordMap.containsKey(strippedWord)) {
+                        List wordList = wordMap.get(strippedWord);
+                        wordList.add(word);
+                        wordMap.put(strippedWord, wordList);
+                    } else {
+                        List wordList = new ArrayList();
+                        wordList.add(word);
+                        wordMap.put(strippedWord, wordList);
+                    }
 
-
-        for(char[] word : guessedLetterLibrary) {
-            int uniques = 0; //Number of unique combinations
-            for(char[] word2 : guessedLetterLibrary) {
-                if(word == word2) {
-                    uniques++;
-                    guessedLetterLibrary.remove(word2);
+                } else {
+                    if (wordMap.containsKey("*")) {
+                        List wordList = wordMap.get("*");
+                        wordList.add(word);
+                        wordMap.put("*", wordList);
+                    } else {
+                        List wordList = new ArrayList();
+                        wordList.add(word);
+                        wordMap.put("*", wordList);
+                    }
                 }
             }
-            System.out.println(uniques);
         }
 
-        return nothing;
+        String biggestFamily = "";
+        int biggestFamilyCount = 0;
+        for (String key:wordMap.keySet()) {
+            if(wordMap.get(key).size() > biggestFamilyCount) {
+                biggestFamilyCount = wordMap.get(key).size();
+                biggestFamily = key;
+            }
+        }
+
+        int total = 0;
+        for (List<String> list : wordMap.values()) {
+            total += list.size();
+        }
+        availableWords = total;
+
+        System.out.println(wordMap.get(biggestFamily));
+        String chosenWord = wordMap.get(biggestFamily).get(0);
+        return chosenWord.toCharArray();
+
     }
 
     public static class ExceedsMaxLengthException extends Exception {
