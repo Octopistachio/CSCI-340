3db39e3d45f7ec90dd67df0122b5d3e0d5ded89c
Matt Wilson <mwilson2250@gmail.com>
2018-04-10 15:49:31 -0500

    LZWCompression Edits

--- LZW/src/LZWCompression.java
+++ LZW/src/LZWCompression.java
@@ -19,6 +19,8 @@ public class LZWCompression {
 
         InitializeDictionary();
 
+        System.out.println(dictionary);
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("File to compress: ");
@@ -32,12 +34,14 @@ public class LZWCompression {
             System.err.println(e);
         }
 
+        System.out.println(dictionary);
+
     }
 
     private static void InitializeDictionary() {
         for (int ch = 0; ch <= 127; ch++) {
             String str = String.valueOf((char) ch);
-            dictionary.put(str, 0);
+            dictionary.put(str, ch);
         }
     }
 
@@ -49,13 +53,16 @@ public class LZWCompression {
 
         while (ch != -1) {
 
+            int index = dictionary.size();
+
             if(dictionary.size() != 256) {
                 if (dictionary.containsKey(str + (char)nextCh)) {
                     str = str + (char) nextCh;
-                    dictionary.put(str, 1);
+                    dictionary.put(str, index);
                     System.out.println("IF: " + str);
                 } else {
-                    dictionary.put(str + (char)nextCh, 0);
+                    System.out.println(dictionary.get(str));
+                    dictionary.put(str + (char)nextCh, index);
                     str = String.valueOf((char)nextCh);
                     System.out.println("ELSE: " + str);
                 }
