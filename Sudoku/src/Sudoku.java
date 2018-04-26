6273599af413671246a1f54db87c532977b348cc
Matt Wilson <mwilson2250@gmail.com>
2018-04-26 14:19:48 -0500

    Added row and column detection

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Sudoku {
 
@@ -28,7 +30,8 @@ public class Sudoku {
         }
 
         readSdk(sdk);
-
+        solve();
+        print(temporary);
     }
 
     private static void readSdk(RandomAccessFile sdk) {
@@ -37,7 +40,6 @@ public class Sudoku {
             int value = sdk.read();
             int row = 0;
             int col = 0;
-
 
             while(value != -1) {
 
@@ -56,16 +58,100 @@ public class Sudoku {
 
                 value = sdk.read();
             }
-
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        temporary = ORIGINAL;
     }
 
     private static void solve() {
-        
+        solve(0, 0);
+    }
+
+    private static int[][] solve(int row, int col) {
+
+        if(ORIGINAL[row][col] == 0)
+            temporary[row][col] = slotCheck(row, col);
+
+        /* Row and Column Counting*/
+        if(row == 8 && col == 8) return temporary; //Base case
+        else {
+            if(col == 8) {
+                row++;
+                col = -1;
+            }
+            col++;
+            return solve(row, col);
+        }
+    }
+
+    private static int slotCheck(int row, int col) {
+        for(int value = 1; value <= 9; value++) {
+            if(rowCheck(row, value) && colCheck(col, value)) {
+                return value;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Checks the other values in the row to
+     * see if this value is able to be used
+     *
+     * @param row
+     * @param value
+     * @return
+     */
+    private static boolean rowCheck(int row, int value) {
+        List<Integer> arr = new ArrayList<>();
+        for(int col = 0; col < temporary[0].length; col++) {
+            if(temporary[row][col] != 0) {
+                arr.add(temporary[row][col]);
+            }
+        }
+
+        return !arr.contains(value);
+    }
+
+    /**
+     * Checks the other values in the column to
+     * see if this value is able to be used
+     *
+     * @param col
+     * @param value
+     * @return
+     */
+    private static boolean colCheck(int col, int value) {
+        List<Integer> arr = new ArrayList<>();
+        for(int row = 0; row < temporary.length; row++) {
+            if(temporary[row][col] != 0) {
+                arr.add(temporary[row][col]);
+            }
+        }
+
+        return !arr.contains(value);
+    }
+
+    /**
+     * Checks the other values in the block to
+     * see if this value is able to be used
+     *
+     * @param row
+     * @param col
+     * @param value
+     * @return
+     */
+    private static boolean blockCheck(int row, int col, int value) {
+        List<Integer> arr = new ArrayList<>();
+        for(int row = 0; row < temporary.length; row++) {
+            if(temporary[row][col] != 0) {
+                arr.add(temporary[row][col]);
+            }
+        }
+
+        return !arr.contains(value);
     }
 
     private static void print(int[][] puzzle) {
